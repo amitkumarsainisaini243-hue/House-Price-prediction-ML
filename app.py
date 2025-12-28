f858b90e1e47156c49749135e2d6142c0d7f81d6
amitkumarsainisaini243-hue <amitkumarsainisaini243@gmail.com>
2025-12-28 15:33:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pickle
 from sklearn.datasets import fetch_california_housing
-st.title('House Price prediction using ML')
+st.title('🏡House Price prediction using ML')
 st.image('https://therealdeal.com/wp-content/uploads/2021/03/CoreLogic-Home-Price-Reports-Highest-Growth-Since-2013.gif')
 df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
@@ -18,6 +18,7 @@ for i in X:
   ans = st.sidebar.slider(f'Select {i} value')
   all_value.append(ans)
 st.write(all_value)
+
 
 
 
