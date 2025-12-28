fdd7f09d71858ab090570964e24db541813500e5
amitkumarsainisaini243-hue <amitkumarsainisaini243@gmail.com>
2025-12-28 15:24:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
-st.sidebar.title(' Select House feature')
+st.sidebar.title('Select house feature')
 st.sidebar.image('https://therealdeal.com/wp-content/uploads/2021/03/CoreLogic-Home-Price-Reports-Highest-Growth-Since-2013.gif')
 all_value = []
 for i in X:
   ans = st.sidebar.slider(f'Select {i} value')
   all_value.append(ans)
 st.write(all_value)
+
 
 
 
