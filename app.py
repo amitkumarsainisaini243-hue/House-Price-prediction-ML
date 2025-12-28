f11e155abedc871ad7a55e42d532c6676c32e428
amitkumarsainisaini243-hue <amitkumarsainisaini243@gmail.com>
2025-12-28 15:41:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ for i in X:
   max_value = int(X[i].max())
   ans = st.sidebar.slider(f'Select {i} value', min_value,max_value)
   all_value.append(ans)
-st.write(all_value)
+#st.write(all_value)
+
 
 
 
