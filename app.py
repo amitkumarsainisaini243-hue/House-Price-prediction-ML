e7a2a46ab807bc9d4a7d111b2b20d8555cdea391
amitkumarsainisaini243-hue <amitkumarsainisaini243@gmail.com>
2025-12-28 14:59:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ from sklearn.preprocessing import StandardScaler
 import pickle
 from sklearn.datasets import fetch_california_housing
 st.title('House Price prediction using ML')
+st.image('https://therealdeal.com/wp-content/uploads/2021/03/CoreLogic-Home-Price-Reports-Highest-Growth-Since-2013.gif')
 
